224394aa4256dd56de8ec01495ae39c361af43ed
Jan-Erik Rediger <jrediger@mozilla.com>
2023-05-04 19:47:55 +0200

    [TEST] an uploader that moves stuff into a file

--- glean-core/rlb/examples/prototype.rs
+++ glean-core/rlb/examples/prototype.rs
@@ -2,13 +2,18 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use std::env;
+use std::fs::File;
 use std::path::PathBuf;
+use std::{
+    env,
+    io::{Read, Write},
+};
 
+use flate2::read::GzDecoder;
 use once_cell::sync::Lazy;
 use tempfile::Builder;
 
-use glean::{private::PingType, ClientInfoMetrics, ConfigurationBuilder};
+use glean::{net, private::PingType, ClientInfoMetrics, ConfigurationBuilder};
 
 pub mod glean_metrics {
     use glean::{private::BooleanMetric, CommonMetricData, Lifetime};
@@ -27,9 +32,50 @@ pub mod glean_metrics {
         });
 }
 
+#[derive(Debug)]
+struct MovingUploader(String);
+
+impl net::PingUploader for MovingUploader {
+    fn upload(
+        &self,
+        url: String,
+        body: Vec<u8>,
+        headers: Vec<(String, String)>,
+    ) -> net::UploadResult {
+        let mut gzip_decoder = GzDecoder::new(&body[..]);
+        let mut s = String::with_capacity(body.len());
+
+        let data = gzip_decoder
+            .read_to_string(&mut s)
+            .ok()
+            .map(|_| &s[..])
+            .or_else(|| std::str::from_utf8(&body).ok())
+            .unwrap();
+
+        let mut out_path = PathBuf::from(&self.0);
+        out_path.push("sent_pings");
+        std::fs::create_dir_all(&out_path).unwrap();
+
+        let docid = url.rsplit('/').next().unwrap();
+        out_path.push(format!("{docid}.json"));
+        let mut fp = File::create(out_path).unwrap();
+
+        // pseudo-JSON, let's hope this works.
+        writeln!(fp, "{{").unwrap();
+        writeln!(fp, "  \"url\": {url},").unwrap();
+        for (key, val) in headers {
+            writeln!(fp, "  \"{key}\": \"{val}\",").unwrap();
+        }
+        writeln!(fp, "}}").unwrap();
+        writeln!(fp, "{data}").unwrap();
+
+        net::UploadResult::http_status(200)
+    }
+}
+
 #[allow(non_upper_case_globals)]
 pub static PrototypePing: Lazy<PingType> =
-    Lazy::new(|| PingType::new("prototype", true, true, vec![]));
+    Lazy::new(|| PingType::new("metrics", true, true, vec![]));
 
 fn main() {
     env_logger::init();
@@ -43,9 +89,11 @@ fn main() {
         root.path().to_path_buf()
     };
 
+    let uploader = MovingUploader(data_path.display().to_string());
     let cfg = ConfigurationBuilder::new(true, data_path, "org.mozilla.glean_core.example")
         .with_server_endpoint("invalid-test-host")
         .with_use_core_mps(true)
+        .with_uploader(uploader)
         .build();
 
     let client_info = ClientInfoMetrics {
@@ -58,6 +106,7 @@ fn main() {
     glean::initialize(cfg, client_info);
 
     glean_metrics::sample_boolean.set(true);
+    glean_metrics::sample_boolean.test_get_value(None);
 
     PrototypePing.submit(None);
 
